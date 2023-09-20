1789ba1a458b5348e18a0c344ab398f8ff60e2aa
Lucas S. Silva <57202598+Coffee-for-Cats@users.noreply.github.com>
2023-09-20 13:26:29 -0300

    comments + -1 score

--- main.mjs
+++ main.mjs
@@ -8,31 +8,29 @@ export const scene = [bird];
 
 const score = document.querySelector('#score');
 
-let distance = 0;
+let distance = -1;
 window.requestAnimationFrame(render);
 function render() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
+    // sleeps for interPipeGap's frames, and them put 2 pipes on screen.
     sleep("putPipes", interPipeGap.gap, function () {
         const { myPipeFloor, myPipeCeil } = invokeDoublePipes();
         scene.push(myPipeFloor, myPipeCeil);
     })
 
+    // removes objects out of bounds.
     scene.forEach(obj => {
         if (bird.contacts(obj) || outOfBounds(bird)) gameOver();
         if (outOfBounds(obj)) {
             scene.splice(i, 1);
         }
     })
+    // execute individual code from each object.
     // needs to be separated, if not, it will execute something already removed
     scene.forEach(obj => obj.alive());
 
-    distance++;
-    canvas.style.backgroundPosition = `${-distance}px 0px`;
-
-    // score printing
-    const cutScore = Math.floor(distance / interPipeGap.gap) - 1;
-    score.textContent = cutScore < 0 ? 0 : cutScore;
+    score();
 
     requestAnimationFrame(render);
 }
@@ -41,4 +39,12 @@ function gameOver() {
     scene.splice(0);
     alert("Game Over!");
     location.reload();
+}
+
+function score() {
+    distance++;
+    canvas.style.backgroundPosition = `${-distance}px 0px`;
+    // score printing
+    const cutScore = Math.floor(distance / interPipeGap.gap);
+    score.textContent = cutScore;
 }
